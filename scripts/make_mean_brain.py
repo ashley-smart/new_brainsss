dd7ad7520940cb4b824224b7bcd48254eaa51e15
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2021-10-08 13:01:03 -0700

    fix indent

--- scripts/make_mean_brain.py
+++ scripts/make_mean_brain.py
@@ -75,7 +75,7 @@ def main(args):
             #printlog(f"COMPLETE | {fly_func_str} | {file} | {brain.shape} --> {meanbrain.shape}")
             printlog(F"meanbrn | COMPLETED | {fly_print} | {func_print} | {file} | {brain.shape} ===> {meanbrain.shape}")
             print(brain.shape[-1]) ### IMPORTANT: for communication to main
-         else:
+        else:
             printlog("COULD NOT FIND STITCHED FILES")
 #         except FileNotFoundError:
 #             printlog(F"Not found (skipping){file:.>{width-20}}")
