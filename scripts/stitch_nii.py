2d9cda1d5cab0a708805461ef500b06fcb9a7bee
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2021-10-08 18:36:30 -0700

    hopefully fixing memory issues

--- scripts/stitch_nii.py
+++ scripts/stitch_nii.py
@@ -16,19 +16,20 @@ date = '20210806'
 dataset_path = "/oak/stanford/groups/trc/data/Ashley2/imports/" + str(date)
 fly_folders = os.listdir(dataset_path)  ## find directory names, they are the fly names
 
-full_brain_ch1 = []
-full_brain_ch2 = []
+
 for fly in fly_folders: 
   directory = os.path.join(dataset_path, fly)
   files = os.listdir(directory)
+  full_brain_ch1 = []
+  full_brain_ch2 = []
   for file in files:
       ## stitch brain ##
       if "channel_1" in file and "nii" in file: 
           brain_ch1 = np.asarray(nib.load(os.path.join(directory, file)).get_data(), dtype='uint16')
           full_brain_ch1.append(brain_ch1)
-      elif "channel_2" in file and "nii" in file:
-          brain_ch2 = np.asarray(nib.load(os.path.join(directory, file)).get_data(), dtype='uint16')
-          full_brain_ch2.append(brain_ch2)
+#       elif "channel_2" in file and "nii" in file:
+#           brain_ch2 = np.asarray(nib.load(os.path.join(directory, file)).get_data(), dtype='uint16')
+#           full_brain_ch2.append(brain_ch2)
           
   #save files        
   if len(full_brain_ch1) > 0:       
@@ -39,7 +40,17 @@ for fly in fly_folders:
       img = nib.Nifti1Image(stitched_brain_ch1, aff)
       img.to_filename(save_file)
       printlog("ch1 brain stitched and saved")
-
+      del full_brain_ch1  #to delete from memory
+      del stitched_brain_ch1 # to delete from memory
+      
+  ### splitting this up to help the memory (hopefully)
+  
+  for file in files:
+      ## stitch brain ##
+      if "channel_2" in file and "nii" in file: 
+          brain_ch2 = np.asarray(nib.load(os.path.join(directory, file)).get_data(), dtype='uint16')
+          full_brain_ch2.append(brain_ch1)
+          
   if len(full_brain_ch2) > 0:
       stitched_brain_ch2 = np.concatenate(full_brain_ch2, axis = -1)
       #save stitched brain
@@ -48,3 +59,5 @@ for fly in fly_folders:
       img = nib.Nifti1Image(stitched_brain_ch2, aff)
       img.to_filename(save_file)
       printlog("ch2 brain stiched and saved")
+      del full_brain_ch2  #to delete from memory
+      del stitched_brain_ch2 # to delete from memory
