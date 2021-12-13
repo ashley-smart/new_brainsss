75e405d1c4d7b5a8c4ba509e1aa264de896346be
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2021-12-13 15:21:57 -0800

    2 day limit

--- scripts/main_not_trc.sh
+++ scripts/main_not_trc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=brainsss_ash
-#SBATCH --time=3-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=18
+#SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=8G
 #SBATCH --output=./logs/mainlog.out
 #SBATCH --open-mode=append
