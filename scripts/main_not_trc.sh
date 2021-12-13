3da90b2e8538dca0f415a6903334aa1d4814313a
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2021-12-13 15:59:45 -0800

    bigmem 1 day

--- scripts/main_not_trc.sh
+++ scripts/main_not_trc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=brainsss_ash
-#SBATCH --time=1-12:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=18
+#SBATCH --cpus-per-task=20
 #SBATCH --partition=bigmem
 #SBATCH --mem-per-cpu=12G
 #SBATCH --output=./logs/mainlog.out
