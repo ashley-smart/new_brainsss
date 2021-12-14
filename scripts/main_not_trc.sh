d014fcbc23d9430c1f1a72d78cc166322a0ca232
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2021-12-13 16:01:58 -0800

    Update main_not_trc.sh

--- scripts/main_not_trc.sh
+++ scripts/main_not_trc.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=brainsss_ash
 #SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
-#SBATCH --partition=bigmem
-#SBATCH --mem-per-cpu=12G
+#SBATCH --cpus-per-task=22
+#SBATCH --partition=normal
+#SBATCH --mem-per-cpu=8G
 #SBATCH --output=./logs/mainlog.out
 #SBATCH --open-mode=append
 #SBATCH --mail-type=ALL
