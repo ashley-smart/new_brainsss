66c46f1065003b607c9f6a483601a4a461496a46
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-01-27 14:41:49 -0800

    switching to owners partition

--- scripts/stitch.sh
+++ scripts/stitch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=stitch_nii
-#SBATCH --partition=trc
-#SBATCH --time=3-00:00:00
+#SBATCH --partition=owners
+#SBATCH --time=2-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=12G
 #SBATCH --output=./logs/mainlog.out
 #SBATCH --open-mode=append
 #SBATCH --mail-type=ALL
