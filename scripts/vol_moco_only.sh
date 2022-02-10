6fa9490bf3e00323cc8bd3d03595c4fb8d3cc3c3
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-02-10 09:48:09 -0800

    Create vol_moco_only.sh

---
+++ scripts/vol_moco_only.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=AS_vol_test
+#SBATCH --time=4-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --partition=trc
+## comment out #SBATCH --mem 260G
+#SBATCH --output=./logs/mainlog.out
+#SBATCH --open-mode=append
+#SBATCH --cpus-per-task=8
+#SBATCH --mail-type=ALL
+
+ml python/3.6.1
+# ml antspy/0.2.2
+date
+python3 -u /home/users/asmart/projects/brainsss_ash/scripts/vol_main.py
