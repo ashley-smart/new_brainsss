2b9dab919ba73e5dac7f8a4d0bf7bef1f514f611
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-02-11 17:34:26 -0800

    Create vol_zscore.py

---
+++ scripts/vol_zscore.py
@@ -0,0 +1,7 @@
+## do zscore on vol brains and using moco h5py files
+##1. open h5py files
+#2. calculate mean brain (whole moco brain)
+#3. calcy=ulate std of brain
+#4. zscore = (vol - mean)/std
+
+# actually run this on chunks of brain rather than one volume so it goes faster
