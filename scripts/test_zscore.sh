8754e8513905c45ebad8e8a4557284cac8a2e794
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-03-06 20:14:47 -0800

    Update test_zscore.sh

--- scripts/test_zscore.sh
+++ scripts/test_zscore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=stitch_nii
+#SBATCH --job-name=testzscore
 #SBATCH --partition=trc
 #SBATCH --time=6-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 
-#SBATCH --output=./logs/stitchlog.out
+#SBATCH --output=./logs/zscoretest.out
 #SBATCH --open-mode=append
 #SBATCH --mail-type=ALL
 
