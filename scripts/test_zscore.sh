bfaf3c7590d86465210b202d2e5176ae612b530a
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-03-06 20:14:07 -0800

    Create test_zscore.sh

---
+++ scripts/test_zscore.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=stitch_nii
+#SBATCH --partition=trc
+#SBATCH --time=6-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+#SBATCH --output=./logs/stitchlog.out
+#SBATCH --open-mode=append
+#SBATCH --mail-type=ALL
+
+ml python/3.6.1
+# ml antspy/0.2.2
+date
+python3 -u /home/users/asmart/projects/new_brainsss/scripts/vol_zscore_test.py
