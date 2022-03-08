981b186257c72920f4dba9c84db505581b0ac2fd
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-03-07 18:33:21 -0800

    Update vol_zscore.py

--- scripts/vol_zscore.py
+++ scripts/vol_zscore.py
@@ -84,7 +84,7 @@ def main(args):
 
             # Append to hdf5 file
             hf['zscore'][...,-1] = each_zscore
-       printlog('ZSCORE complete')
+        printlog('ZSCORE complete')
         
 
     
