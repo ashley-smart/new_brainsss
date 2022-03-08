0e71343409326f1717cb738575f132566ec75982
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-03-08 08:12:59 -0800

    Update vol_main.py

--- scripts/vol_main.py
+++ scripts/vol_main.py
@@ -15,14 +15,14 @@ import gc
 
 modules = 'gcc/6.3.0 python/3.6.1 py-numpy/1.14.3_py36 py-pandas/0.23.0_py36 viz py-scikit-learn/0.19.1_py36' 
 
-logfile = './logs/' + time.strftime("%Y%m%d-%H%M%S") + '.txt'
-printlog = getattr(brainsss.Printlog(logfile=logfile), 'print_to_log')
-sys.stderr = brainsss.Logger_stderr_sherlock(logfile)
+
 
 scripts_path = "/home/users/asmart/projects/new_brainsss/scripts"
 com_path = "/home/users/asmart/projects/new_brainsss/scripts/com"
 
 date = '20210806'
+
+dataset_path = "/oak/stanford/groups/trc/data/Ashley2/imports/" + str(date)
 
 mem = 4
 time = 144 #time in hours before it stops running
@@ -30,7 +30,21 @@ width = 120 # width of print log
 nodes = 1 # 1 or 2
 nice = True #True # true to lower priority of jobs. ie, other users jobs go first
 
-dataset_path = "/oak/stanford/groups/trc/data/Ashley2/imports/" + str(date)
+
+
+#####################
+### Setup logging ###
+#####################
+
+logfile = './logs/' + time.strftime("%Y%m%d-%H%M%S") + '.txt'
+printlog = getattr(brainsss.Printlog(logfile=logfile), 'print_to_log')
+sys.stderr = brainsss.Logger_stderr_sherlock(logfile)
+
+
+###################################
+## look for flies in date folder ##
+###################################
+
 flies_temp = os.listdir(dataset_path)  ## find directory names, they are the fly names
 #to sort out non-fly directories (issue if I ever label a file with fly but I can't get isdir to work.)
 flies = []
@@ -52,7 +66,7 @@ printlog("")
 # ######################
 # ### Test vol moco ####
 # #######################
-# printlog(f"\n{'   vol by vol test   ':=^{width}}")
+# printlog(f"\n{'   vol by vol moco test   ':=^{width}}")
 # job_ids = []
 # for fly in flies:
 #     directory = os.path.join(dataset_path, fly)
@@ -73,12 +87,13 @@ printlog("")
 ######################
 ### Test vol zscore ####
 #######################
-printlog(f"\n{'   vol by vol test   ':=^{width}}")
+printlog(f"\n{'   vol by vol zscore test   ':=^{width}}")
+moco_names = ['MOCO_ch1.h5', 'MOCO_ch2.h5']   #run zscore on moco h5 files
 job_ids = []
 for fly in flies:
     directory = os.path.join(dataset_path, fly)
     save_path = directory  #could have it save in a different folder in the future
-    args = {'logfile': logfile, 'directory': directory, 'smooth': False, 'colors': ['green'], 'file_names': ['MOCO_ch1.h5', 'MOCO_ch2.h5'], 'save_path': save_path}
+    args = {'logfile': logfile, 'directory': directory, 'smooth': False, 'colors': ['green'], 'file_names': moco_names, 'save_path': save_path}
     script = 'vol_zscore.py'
     printlog(os.path.join(scripts_path, script))
     job_id = brainsss.sbatch(jobname='volzscore',
@@ -87,12 +102,15 @@ for fly in flies:
                          args=args,
                          logfile=logfile, time=time, mem=mem, nice=nice, nodes=nodes)
     job_ids.append(job_id)
+    printlog("fly started")
 
 for job_id in job_ids:
     brainsss.wait_for_job(job_id, logfile, com_path)
-    
-    
-    
+
+############
+### Done ###
+############
+
 time.sleep(30) # to allow any final printing
 day_now = datetime.datetime.now().strftime("%B %d, %Y")
 time_now = datetime.datetime.now().strftime("%I:%M:%S %p")
