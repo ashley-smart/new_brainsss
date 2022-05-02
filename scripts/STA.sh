590fc6e6a8956a1e9772fe1d8806d5f4f8e2b674
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-05-01 18:04:20 -0700

    Create STA.sh

---
+++ scripts/STA.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=STA_anticipation
+#SBATCH --time=7-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --partition=trc
+## comment out #SBATCH --mem 260G
+#SBATCH --output=./logs/mainlogSTA.out
+#SBATCH --open-mode=append
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=ALL
+
+ml python/3.6.1
+# ml antspy/0.2.2
+date
+python3 -u /home/users/asmart/projects/new_brainsss/scripts/STA_anticipation.py
