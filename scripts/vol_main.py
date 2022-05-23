1aaf48d1f063fc1661d65d702125da28a11a8295
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-05-23 16:23:41 -0700

    Update vol_main.py

--- scripts/vol_main.py
+++ scripts/vol_main.py
@@ -21,7 +21,7 @@ modules = 'gcc/6.3.0 python/3.6.1 py-numpy/1.14.3_py36 py-pandas/0.23.0_py36 viz
 scripts_path = "/home/users/asmart/projects/new_brainsss/scripts"
 com_path = "/home/users/asmart/projects/new_brainsss/scripts/com"
 
-date = '20210802'
+date = '20210719'
 
 dataset_path = "/oak/stanford/groups/trc/data/Ashley2/imports/" + str(date)
 #dataset_path = "/oak/stanford/groups/trc/data/krave/bruker_data/imports/" + str(date)
