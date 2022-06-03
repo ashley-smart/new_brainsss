96eda659083cac1fdbe22fcbff253550849464ff
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-06-03 08:15:39 -0700

    Update stitch.sh

--- scripts/stitch.sh
+++ scripts/stitch.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 
-#SBATCH --output=./logs/stitchlog.out
+#SBATCH --output=./logs/stitchlog2.out
 #SBATCH --open-mode=append
 #SBATCH --mail-type=ALL
 
