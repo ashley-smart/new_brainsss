b34b2873cee913e1256e12df02c4a6b5c4ee62f4
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-06-06 16:50:50 -0700

    Update vol_main.py

--- scripts/vol_main.py
+++ scripts/vol_main.py
@@ -34,7 +34,7 @@ for date in dates:
 
     mem = 4
     high_pass_mem = 6
-    runtime = 144 #time in hours before it stops running  use 48 for normal partition
+    runtime = 48 #144 #time in hours before it stops running  use 48 for normal partition
     width = 120 # width of print log
     nodes = 1 # 1 or 2
     nice = True #True # true to lower priority of jobs. ie, other users jobs go first
