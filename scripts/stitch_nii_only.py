7abdf587fb3cb6fdf391425e3d34ff165a49cc64
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-02-01 15:55:59 -0800

    Update stitch_nii_only.py

--- scripts/stitch_nii_only.py
+++ scripts/stitch_nii_only.py
@@ -14,7 +14,7 @@ import gc
 
 #get to files
 #date = '20211215'
-dates = ['20230130'] 
+dates = ['20230127'] 
 for date in dates:
   print('STARTING DATE:', str(date))
   dataset_path = "/oak/stanford/groups/trc/data/Ashley2/imports/" + str(date)
