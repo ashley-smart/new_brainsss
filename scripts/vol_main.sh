f2b40998f9ff52a0e84a584aea89467ed799623e
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-04-11 15:44:45 -0700

    reverting args from shell

--- scripts/vol_main.sh
+++ scripts/vol_main.sh
@@ -8,10 +8,9 @@
 #SBATCH --open-mode=append
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
-# Params: (1) experiment_date
 
 ml python/3.6.1
 # ml antspy/0.2.2
 date
-experiment_date=$1
-python3 -u /home/users/asmart/projects/new_brainsss/scripts/vol_main.py $experiment_date
+
+python3 -u /home/users/asmart/projects/new_brainsss/scripts/vol_main.py 
