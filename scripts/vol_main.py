0411504f1888acf77294c830b8353516d71ce807
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-06-08 15:03:02 -0700

    fix

--- scripts/vol_main.py
+++ scripts/vol_main.py
@@ -164,7 +164,7 @@ for date in dates:
                             script=os.path.join(scripts_path, script),
                             modules=modules,
                             args=args,
-                            logfile=logfile, time=5, mem=18, nice=nice, nodes=nodes, global_resources=True)
+                            logfile=logfile, time=5, mem=18, nice=nice, nodes=nodes)
         brainsss.wait_for_job(job_id, logfile, com_path)
 
 
