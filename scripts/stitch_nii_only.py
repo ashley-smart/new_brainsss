62ac852e546f49410dd4e2103fb2ea5359fd2590
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-06-08 11:27:29 -0700

    fix punctuatuion

--- scripts/stitch_nii_only.py
+++ scripts/stitch_nii_only.py
@@ -64,15 +64,15 @@ for date in dates:
     for number in values_sorted:
        sorted_channel_1_list.append([file for file in channel_1_list if 's' + str(number) + '.nii' in file])
        sorted_channel_2_list.append([file for file in channel_2_list if 's' + str(number) + '.nii' in file])
-    sorted_channel_1_list = sorted_channel_1_list[0] #nested list
-    sorted_channel_2_list = sorted_channel_2_list[0]
+    sorted_channel_1_list = sorted_channel_1_list
+    sorted_channel_2_list = sorted_channel_2_list
     print('sorted_channel_1_list', sorted_channel_1_list)
     print('sorted_channel_2_list', sorted_channel_2_list)
 
     #iterate through sorted list and append files
     for i in sorted_channel_1_list: 
-        print('ch1 file: ', i)
-        brain_ch1 = np.asarray(nib.load(os.path.join(directory, i)).get_data(), dtype='uint16')
+        print('ch1 file: ', i[0])
+        brain_ch1 = np.asarray(nib.load(os.path.join(directory, i[0])).get_data(), dtype='uint16')
         #print('shape of brain file: ', np.shape(brain_ch1))
         full_brain_ch1.append(brain_ch1)
 
