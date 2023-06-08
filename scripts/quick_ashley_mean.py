60098f50f6ca61386b3941296b5674a0cbea80f7
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-06-08 12:39:08 -0700

    fix brainsss import

--- scripts/quick_ashley_mean.py
+++ scripts/quick_ashley_mean.py
@@ -4,8 +4,12 @@ import os
 import re
 import json
 import textwrap
-import brainsss
+
 import argparse
+
+sys.path.append(os.path.split(os.path.dirname(__file__))[0])
+print(sys.path)
+import brainsss
 
 modules = 'gcc/6.3.0 python/3.6 py-numpy/1.14.3_py36 py-pandas/0.23.0_py36 viz py-scikit-learn/0.19.1_py36 antspy/0.2.2'
 
