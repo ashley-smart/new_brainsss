a4278f203ff8817a636038400f73c9bade5090c4
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-06-15 16:19:00 -0700

    adjust mean

--- scripts/make_mean_brain.py
+++ scripts/make_mean_brain.py
@@ -27,38 +27,41 @@ def main(args):
     
 
     for file in files:
-        try:
+        # try:
+
             ### make mean ###
-            printlog(f'current file attempting: {file}')
-            full_path = os.path.join(directory, file)
-            if full_path.endswith('stitched.nii'):
-                brain = np.asarray(nib.load(full_path).get_data(), dtype='uint16')
-            elif full_path.endswith('.h5'):
-                with h5py.File(full_path, 'r') as hf:
-                    brain = np.asarray(hf['data'][:], dtype='uint16')
+        printlog(f'current file attempting: {file}')
+        full_path = os.path.join(directory, file)
+        if full_path.endswith('stitched.nii') and 'mean' not in full_path:
+            printlog(f'found potental file {file}')
+            brain = np.asarray(nib.load(full_path).get_data(), dtype='uint16')
+        ##this condition doesn't happen for my flies. Uncomment if I want average from moco
+        # elif full_path.endswith('.h5'):
+        #     with h5py.File(full_path, 'r') as hf:
+        #         brain = np.asarray(hf['data'][:], dtype='uint16')
 
-                if meanbrain_n_frames is not None:
-                    # average over first meanbrain_n_frames frames
-                    meanbrain = np.mean(brain[...,:int(meanbrain_n_frames)], axis=-1)
-                else: # average over all frames
-                    meanbrain = np.mean(brain, axis=-1)
+            if meanbrain_n_frames is not None:
+                # average over first meanbrain_n_frames frames
+                meanbrain = np.mean(brain[...,:int(meanbrain_n_frames)], axis=-1)
+            else: # average over all frames
+                meanbrain = np.mean(brain, axis=-1)
 
-                ### Save ###
-                save_file = os.path.join(directory, file[:-4] + '_mean.nii')
-                aff = np.eye(4)
-                img = nib.Nifti1Image(meanbrain, aff)
-                img.to_filename(save_file)
+            ### Save ###
+            save_file = os.path.join(directory, file[:-4] + '_mean.nii')
+            aff = np.eye(4)
+            img = nib.Nifti1Image(meanbrain, aff)
+            img.to_filename(save_file)
 
-                fly_func_str = ('|').join(directory.split('/')[-3:-1])
-                fly_print = directory.split('/')[-3]
-                func_print = directory.split('/')[-2]
-                #printlog(f"COMPLETE | {fly_func_str} | {file} | {brain.shape} --> {meanbrain.shape}")
-                printlog(F"meanbrn | COMPLETED | {fly_print} | {func_print} | {file} | {brain.shape} ===> {meanbrain.shape}")
-                print(brain.shape[-1]) ### IMPORTANT: for communication to main
-                brain = None
-        except FileNotFoundError:
-            printlog(F"Not found (skipping){file:.>{width-20}}")
-            #printlog(f'{file} not found.')
+            fly_func_str = ('|').join(directory.split('/')[-3:-1])
+            fly_print = directory.split('/')[-3]
+            func_print = directory.split('/')[-2]
+            #printlog(f"COMPLETE | {fly_func_str} | {file} | {brain.shape} --> {meanbrain.shape}")
+            printlog(F"meanbrn | COMPLETED | {fly_print} | {func_print} | {file} | {brain.shape} ===> {meanbrain.shape}")
+            print(brain.shape[-1]) ### IMPORTANT: for communication to main
+            brain = None
+        # except FileNotFoundError:
+        #     printlog(F"Not found (skipping){file:.>{width-20}}")
+        #     #printlog(f'{file} not found.')
 
 if __name__ == '__main__':
     main(json.loads(sys.argv[1]))
