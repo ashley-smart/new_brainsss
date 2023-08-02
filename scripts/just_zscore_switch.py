a5aa5372dbb5d23a3c1c45676db985de9e6d5db0
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2023-08-01 20:42:33 -0700

    more mem

--- scripts/just_zscore_switch.py
+++ scripts/just_zscore_switch.py
@@ -30,7 +30,7 @@ for date in dates:
     #dataset_path = "/oak/stanford/groups/trc/data/krave/bruker_data/imports/" + str(date)
 
 
-    mem = 8
+    mem = 16
     high_pass_mem = 6
     runtime = 48 #144 #time in hours before it stops running  use 48 for normal partition
     width = 120 # width of print log
