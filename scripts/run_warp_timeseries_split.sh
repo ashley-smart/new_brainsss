d2bda0ecce1ff99ee9613c48d62c1ef96a39978d
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2024-05-20 12:27:42 -0700

    trying split brain for warp

---
+++ scripts/run_warp_timeseries_split.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=warp_i
+#SBATCH --time=7-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --partition=trc
+## comment out #SBATCH --mem 260G
+#SBATCH --output=./logs/warp_mainlog.out
+#SBATCH --open-mode=append
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+
+ml python/3.6.1
+# ml antspy/0.2.2
+date
+
+python3 -u /home/users/asmart/projects/new_brainsss/scripts/run_warp_timeseries_split.py 
