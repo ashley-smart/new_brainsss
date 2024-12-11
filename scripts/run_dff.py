bf9ea81d599cb55ddff5584c87f003ce32668b3b
ashley-smart <ash.smart@gmail.com>
2024-12-11 13:40:10 -0800

    new date dff

--- scripts/run_dff.py
+++ scripts/run_dff.py
@@ -24,7 +24,7 @@ com_path = "/home/users/asmart/projects/new_brainsss/scripts/com"
 
 
 
-dates = ['20230616'] 
+dates = ['20240830'] 
 for date in dates:
 
     dataset_path = "/oak/stanford/groups/trc/data/Ashley2/imports/" + str(date)
